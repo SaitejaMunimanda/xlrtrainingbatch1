eef6fef990079ae4088d21ee2f31381ebd42c94b
SaitejaMunimanda <saitejamunimanda@gmail.com>
2025-01-20 18:10:50 +0530

    Add files via upload

---
+++ Core Java/Demo003.java
@@ -0,0 +1,17 @@
+public class Demo003 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        if ((a>b) && (a>b)) {
+            System.out.println(a+" is greater");
+        }
+        else if ((b>a) && (b>c)) {
+            System.out.println(b+" is greater");
+        }
+        else {
+            System.out.println(c+" is greater");
+        }
+    
+}
+}
